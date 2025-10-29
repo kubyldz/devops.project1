70eaa6af86302e34bd84966ebc8c47041ce6f3e8
Bercis Kübra Yıldız <128314142+kubyldz@users.noreply.github.com>
2025-10-29 13:05:13 +0300

    first commit

---
+++ src/test/java/com/example/devops/project1/ApplicationTests.java
@@ -0,0 +1,27 @@
+package com.example.devops.project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HelloController.class)
+class ApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testHelloEndpoint() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello DevOps Pipeline!"));
+    }
+}
